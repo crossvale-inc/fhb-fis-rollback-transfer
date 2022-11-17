82dae63b534693f0172e510f32d0aedafb5cdd39
Jjgarciag <90756477+Jjgarciag@users.noreply.github.com>
2022-11-17 14:08:55 +0100

    updated rollback route

--- src/main/java/com/fhb/fis/rollback/transfer/routes/DepositRollbackRouteBuilder.java
+++ src/main/java/com/fhb/fis/rollback/transfer/routes/DepositRollbackRouteBuilder.java
@@ -51,57 +51,58 @@ public class DepositRollbackRouteBuilder extends RouteBuilder {
         .to("direct:postOnlineDollarTransactions")//enrich keep headers and change body
         //In case of error, get error code
         .filter().jsonpath("$.fault", true)
-            .setProperty(Constants.FAULT_CODE).jsonpath("$.fault.code", true)
-            .setProperty(Constants.FAULT_MESSAGE).jsonpath("$.fault.message", true)
+        .setProperty(Constants.FAULT_CODE).jsonpath("$.fault.code", true)
+        .setProperty(Constants.FAULT_MESSAGE).jsonpath("$.fault.message", true)
             .setProperty(Constants.STATUS_CODE).jsonpath("$.fault.transportErrorCode", true)
             .log(LoggingLevel.ERROR, LOGGER, "{\"Stage\":\"Error\", \"logMessage\": \"Received error response from online dollar transaction service. HTTP=${exchangeProperty[statusCode]}, Error=${exchangeProperty[faultCode]}-${exchangeProperty[faultMessage]}\", \"method\":\"/${header[appPath]}.\", \"channel\":\"${header["+CommonInputHeader.APP_CODE+"]}\", \"customerNr\":\"${property[customerNr]}\", \"amount\":\"${property[amount]}\", \"fromAccount\":\"${property[fromAccount]}\", \"fromAccountType\":\"${property[fromAccountType]}\", \"toAccount\":\"${property[toAccount]}\", \"ToAccountType\":\"${property[toAccountType]}\", \"platform\":\"${header["+Constants.CARD_PLATFORM_PROPERTY+"]}\", \"debitBicId\":\"${property[debitHardPostingInternalId]}\", \"creditBicId\":\"${property[creditHardPostingInternalId]}\", \"statusCode\":\"${header["+Constants.STATUS_CODE+"]}\",\"ibsOperationId\":\"${property["+Constants.IBS_OPERATION_HEADER+"]}\", \"errorCode\":\"${header["+Constants.FAULT_CODE+"]}\", \"errorMessage\":\"${header["+Constants.FAULT_MESSAGE+"]}\"}")
             .setProperty("creditDepositRollbackFailedError").jsonpath("$.fault.code", true, String.class)
             .log(LoggingLevel.ERROR, LOGGER, "{\"Stage\":\"Error\", \"logMessage\": \"Exception thrown by the online dollar transaction service: rollback process failure. HTTP=${exchangeProperty[statusCode]}, Error=${exchangeProperty[faultCode]}-${exchangeProperty[faultMessage]}\", \"method\":\"/${header[appPath]}.\", \"channel\":\"${header["+CommonInputHeader.APP_CODE+"]}\", \"customerNr\":\"${property[customerNr]}\", \"amount\":\"${property[amount]}\", \"fromAccount\":\"${property[fromAccount]}\", \"fromAccountType\":\"${property[fromAccountType]}\", \"toAccount\":\"${property[toAccount]}\", \"ToAccountType\":\"${property[toAccountType]}\", \"platform\":\"${header["+Constants.CARD_PLATFORM_PROPERTY+"]}\", \"debitBicId\":\"${property[debitHardPostingInternalId]}\", \"creditBicId\":\"${property[creditHardPostingInternalId]}\", \"statusCode\":\"${header["+Constants.STATUS_CODE+"]}\",\"ibsOperationId\":\"${property["+Constants.IBS_OPERATION_HEADER+"]}\", \"errorCode\":\"${header["+Constants.FAULT_CODE+"]}\", \"errorMessage\":\"${header["+Constants.FAULT_MESSAGE+"]}\"}")
-        .end()
-        //Analyze response not an error
-        .choice()
-        .when().simple("'{{ibs.employee.controlled.error}}' contains ${property.creditDepositRollbackFailedError}")
+            .end()
+            //Analyze response not an error
+            .choice()
+            .when().simple("'{{ibs.employee.controlled.error}}' contains ${property.creditDepositRollbackFailedError}")
             .log(LoggingLevel.INFO, LOGGER, "Online dollar transaction failed, retry due to controlled FHB employee error")
             .to(DO_ROLLBACK_EMPLOYEE_CREDIT_DEPOSIT_URI)
-        .endChoice()
-        .when().jsonpath("$.fault", true)
+            .endChoice()
+            .when().jsonpath("$.fault", true)
             .setProperty(Constants.FAULT_CODE).jsonpath("$.fault.code", true)
             .setProperty(Constants.FAULT_MESSAGE).jsonpath("$.fault.message", true)
             .setProperty(Constants.STATUS_CODE).jsonpath("$.fault.transportErrorCode", true)
             .setProperty(Constants.FAULT_STAGE,constant(ErrorCode.ROLLBACK_CREDIT_POST_FAILED))
             .log(LoggingLevel.ERROR, LOGGER, "{\"Stage\":\"Error\", \"logMessage\": \"Exception thrown by the online dollar transaction service: rollback process failure. HTTP=${exchangeProperty[statusCode]}, Error=${exchangeProperty[faultCode]}-${exchangeProperty[faultMessage]}\", \"method\":\"/${header[appPath]}.\", \"channel\":\"${header["+CommonInputHeader.APP_CODE+"]}\", \"customerNr\":\"${property[customerNr]}\", \"amount\":\"${property[amount]}\", \"fromAccount\":\"${property[fromAccount]}\", \"fromAccountType\":\"${property[fromAccountType]}\", \"toAccount\":\"${property[toAccount]}\", \"ToAccountType\":\"${property[toAccountType]}\", \"platform\":\"${header["+Constants.CARD_PLATFORM_PROPERTY+"]}\", \"debitBicId\":\"${property[debitHardPostingInternalId]}\", \"creditBicId\":\"${property[creditHardPostingInternalId]}\", \"statusCode\":\"${header["+Constants.STATUS_CODE+"]}\",\"ibsOperationId\":\"${property["+Constants.IBS_OPERATION_HEADER+"]}\", \"errorCode\":\"${header["+Constants.FAULT_CODE+"]}\", \"errorMessage\":\"${header["+Constants.FAULT_MESSAGE+"]}\"}")
-        .endChoice()
-        .otherwise()
-            .log(LoggingLevel.INFO, LOGGER, "Deposit rollback executed")
-        .endChoice()
-        .end()
-        //If credit rollback executed, do not mark credit posting table as FAILED, rollback was successful
-        .log(LoggingLevel.INFO, LOGGER, "Deposit Credit rollback executed successfully, posting table record will not be marked as 'FAILED' if rollback process fails")
-        .setProperty(Constants.MARK_POSTING_TABLE_CREDIT_AS_FAILED_PROPERTY, constant(false));
-    }
-    
-    public void fromDoRollbackCreditEmployee(RouteDefinition fromEntry){
-        //Do deposit debit rollback for employee
-        fromEntry
-        .routeId(DO_ROLLBACK_EMPLOYEE_CREDIT_DEPOSIT_ID)
-        .log(LoggingLevel.INFO, LOGGER, "Executing Deposit Credit Employee Rollback")
-        .setBody(header(Constants.REQUEST_BODY))
-        .to("{{jolt.transfer.to.dollar.transactions.credit.employee.rollback.request.spec}}")
-        .to("direct:postOnlineDollarTransactions")
-        //Analyze response not an error
-        .choice()
-            .when().jsonpath("$.fault", true)
-                .log(LoggingLevel.ERROR, LOGGER, "{\"Stage\":\"Error\", \"logMessage\": \"Exception thrown by the online dollar transaction service: rollback process failure. HTTP=${exchangeProperty[statusCode]}, Error=${exchangeProperty[faultCode]}-${exchangeProperty[faultMessage]}\", \"method\":\"/${header[appPath]}.\", \"channel\":\"${header["+CommonInputHeader.APP_CODE+"]}\", \"customerNr\":\"${property[customerNr]}\", \"amount\":\"${property[amount]}\", \"fromAccount\":\"${property[fromAccount]}\", \"fromAccountType\":\"${property[fromAccountType]}\", \"toAccount\":\"${property[toAccount]}\", \"ToAccountType\":\"${property[toAccountType]}\", \"platform\":\"${header["+Constants.CARD_PLATFORM_PROPERTY+"]}\", \"debitBicId\":\"${property[debitHardPostingInternalId]}\", \"creditBicId\":\"${property[creditHardPostingInternalId]}\", \"statusCode\":\"${header["+Constants.STATUS_CODE+"]}\",\"ibsOperationId\":\"${property["+Constants.IBS_OPERATION_HEADER+"]}\", \"errorCode\":\"${header["+Constants.FAULT_CODE+"]}\", \"errorMessage\":\"${header["+Constants.FAULT_MESSAGE+"]}\"}")
             .endChoice()
             .otherwise()
-                .log(LoggingLevel.INFO, LOGGER, "Deposit rollback executed")
+            .log(LoggingLevel.INFO, LOGGER, "Deposit rollback executed")
             .endChoice()
-        .end();
-    }
-    
-    //Rollback debit
-    public void fromDoRollbackDebitDeposit(RouteDefinition fromEntry){
-        fromEntry
+            .end()
+            //If credit rollback executed, do not mark credit posting table as FAILED, rollback was successful
+            .log(LoggingLevel.INFO, LOGGER, "Deposit Credit rollback executed successfully, posting table record will not be marked as 'FAILED' if rollback process fails")
+            .setProperty(Constants.MARK_POSTING_TABLE_CREDIT_AS_FAILED_PROPERTY, constant(false));
+        }
+        
+        public void fromDoRollbackCreditEmployee(RouteDefinition fromEntry){
+            //Do deposit debit rollback for employee
+            fromEntry
+            .routeId(DO_ROLLBACK_EMPLOYEE_CREDIT_DEPOSIT_ID)
+            .log(LoggingLevel.INFO, LOGGER, "Executing Deposit Credit Employee Rollback")
+            .setBody(header(Constants.REQUEST_BODY))
+            .to("{{jolt.transfer.to.dollar.transactions.credit.employee.rollback.request.spec}}")
+            .to("direct:postOnlineDollarTransactions")
+            //Analyze response not an error
+            .choice()
+            .when().jsonpath("$.fault", true)
+            .log(LoggingLevel.ERROR, LOGGER, "{\"Stage\":\"Error\", \"logMessage\": \"Exception thrown by the online dollar transaction service: rollback process failure. HTTP=${exchangeProperty[statusCode]}, Error=${exchangeProperty[faultCode]}-${exchangeProperty[faultMessage]}\", \"method\":\"/${header[appPath]}.\", \"channel\":\"${header["+CommonInputHeader.APP_CODE+"]}\", \"customerNr\":\"${property[customerNr]}\", \"amount\":\"${property[amount]}\", \"fromAccount\":\"${property[fromAccount]}\", \"fromAccountType\":\"${property[fromAccountType]}\", \"toAccount\":\"${property[toAccount]}\", \"ToAccountType\":\"${property[toAccountType]}\", \"platform\":\"${header["+Constants.CARD_PLATFORM_PROPERTY+"]}\", \"debitBicId\":\"${property[debitHardPostingInternalId]}\", \"creditBicId\":\"${property[creditHardPostingInternalId]}\", \"statusCode\":\"${header["+Constants.STATUS_CODE+"]}\",\"ibsOperationId\":\"${property["+Constants.IBS_OPERATION_HEADER+"]}\", \"errorCode\":\"${header["+Constants.FAULT_CODE+"]}\", \"errorMessage\":\"${header["+Constants.FAULT_MESSAGE+"]}\"}")
+            .endChoice()
+            .otherwise()
+            .log(LoggingLevel.INFO, LOGGER, "Deposit rollback executed")
+            .endChoice()
+            .end();
+        }
+        
+        //Rollback debit
+        public void fromDoRollbackDebitDeposit(RouteDefinition fromEntry){
+            fromEntry
+            .log(LoggingLevel.INFO,LOGGER,"{\"Stage\":\"Initializing\",\"method\":\"rollbackCreditDeposit\"}")
         .to("{{jolt.transfer.to.dollar.transactions.debit.rollback.request.spec}}")
         .to("direct:postOnlineDollarTransactions")
         //In case of error, get error code
--- src/main/java/com/fhb/fis/rollback/transfer/routes/ReceiveKafkaEventRouteBuilder.java
+++ src/main/java/com/fhb/fis/rollback/transfer/routes/ReceiveKafkaEventRouteBuilder.java
@@ -20,12 +20,19 @@ public class ReceiveKafkaEventRouteBuilder extends OABServiceRouteBuilder{
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ReceiveKafkaEventRouteBuilder.class);
 	
+	public static final String ROLLBACK_OPERATION_ID = "rollback";
+	
+	public static final String ROLLBACK_ENTRY_URI = "direct:"+ROLLBACK_OPERATION_ID;
+	public static final String ROLLBACK_ENTRY_ID = "R02_"+ROLLBACK_OPERATION_ID;
+	
 	@Override
 	public void configure() throws Exception {
 		this.errorHandler(noErrorHandler());
 		super.configure();
 		configureEntryRoute(from(KAFKA_ENTRY_URI)
 				.routeId(KAFKA_ENTRY_ID));
+		configureExecuteRollbackRoute(from(ROLLBACK_ENTRY_URI)
+				.routeId(ROLLBACK_ENTRY_ID));
 	}
 
 	@Override
@@ -35,34 +42,39 @@ public class ReceiveKafkaEventRouteBuilder extends OABServiceRouteBuilder{
 		.choice()
 			.when(method(MessageFilterDate.class,"isAfterHeaderLimit").isEqualTo(Boolean.FALSE))//Kafka to envelop wrapper
 				.log(LoggingLevel.INFO,LOGGER,"Receiving Kafka event")
-					.to(KAFKA_ENTRY_URI)
-				.endChoice()
-			.end()
-			.filter(method(MessageFilterDate.class,"isAfterHeaderLimit").isEqualTo(Boolean.TRUE))//Kafka to envelop wrapper
-			.log(LoggingLevel.INFO,LOGGER,"Receiving Kafka event headers:${headers}, body: ${body}")
-				.choice()
-					.when(PredicateBuilder.and(header(KafkaConstants.ACCCOUNT_TYPE).isEqualTo(KafkaConstants.DEPOSITS),header(KafkaConstants.TYPE).isEqualTo(KafkaConstants.CREDIT)))
-						.to(DepositRollbackRouteBuilder.DO_ROLLBACK_CREDIT_DEPOSIT_URI)
-					.endChoice()
-					.when(PredicateBuilder.and(header(KafkaConstants.ACCCOUNT_TYPE).isEqualTo(KafkaConstants.DEPOSITS),header(KafkaConstants.TYPE).isEqualTo(KafkaConstants.DEBIT)))
-						.to(DepositRollbackRouteBuilder.DO_ROLLBACK_DEBIT_DEPOSIT_URI)
-					.endChoice()
-					.when(PredicateBuilder.and(header(KafkaConstants.ACCCOUNT_TYPE).isEqualTo(KafkaConstants.LOANS),header(KafkaConstants.TYPE).isEqualTo(KafkaConstants.CREDIT)))
-						.to("direct:LoansNoteRouteBuilder.DO_ROLLBACK_CREDIT_DEPOSIT_URI")
-					.endChoice()
-						.when(PredicateBuilder.and(header(KafkaConstants.ACCCOUNT_TYPE).isEqualTo(KafkaConstants.LOANS),header(KafkaConstants.TYPE).isEqualTo(KafkaConstants.DEBIT)))
-						.to("direct:LoansNoteRouteBuilder.DO_ROLLBACK_CREDIT_DEPOSIT_URI")
-					.endChoice()
-						.when(PredicateBuilder.and(header(KafkaConstants.ACCCOUNT_TYPE).isEqualTo(KafkaConstants.LOANS_NOTE),header(KafkaConstants.TYPE).isEqualTo(KafkaConstants.CREDIT)))
-						.to("direct:LoansNoteRouteBuilder.DO_ROLLBACK_CREDIT_DEPOSIT_URI")
-					.endChoice()
-						.when(PredicateBuilder.and(header(KafkaConstants.ACCCOUNT_TYPE).isEqualTo(KafkaConstants.LOANS_NOTE),header(KafkaConstants.TYPE).isEqualTo(KafkaConstants.DEBIT)))
-						.to("direct:LoansNoteRouteBuilder.DO_ROLLBACK_CREDIT_DEPOSIT_URI")
-					.endChoice()
-				.end()
-			.end()
+				.to(KAFKA_ENTRY_URI)
+			.endChoice()
+			.otherwise()
+				.to(ROLLBACK_ENTRY_URI)
+			.endChoice()
+		.end()
 			.log(LoggingLevel.INFO,LOGGER,"Kafka event processed")
 			;
+	}
+
+	public void configureExecuteRollbackRoute(RouteDefinition fromEntry){
+		fromEntry
+		.log(LoggingLevel.INFO,LOGGER,"Receiving Kafka event headers:${headers}, body: ${body}")
+		.choice()
+			.when(PredicateBuilder.and(header(KafkaConstants.ACCCOUNT_TYPE).isEqualTo(KafkaConstants.DEPOSITS),header(KafkaConstants.TYPE).isEqualTo(KafkaConstants.CREDIT)))
+				.to(DepositRollbackRouteBuilder.DO_ROLLBACK_CREDIT_DEPOSIT_URI)
+			.endChoice()
+			.when(PredicateBuilder.and(header(KafkaConstants.ACCCOUNT_TYPE).isEqualTo(KafkaConstants.DEPOSITS),header(KafkaConstants.TYPE).isEqualTo(KafkaConstants.DEBIT)))
+				.to(DepositRollbackRouteBuilder.DO_ROLLBACK_DEBIT_DEPOSIT_URI)
+			.endChoice()
+			.when(PredicateBuilder.and(header(KafkaConstants.ACCCOUNT_TYPE).isEqualTo(KafkaConstants.LOANS),header(KafkaConstants.TYPE).isEqualTo(KafkaConstants.CREDIT)))
+				.to("direct:LoansNoteRouteBuilder.DO_ROLLBACK_CREDIT_DEPOSIT_URI")
+			.endChoice()
+				.when(PredicateBuilder.and(header(KafkaConstants.ACCCOUNT_TYPE).isEqualTo(KafkaConstants.LOANS),header(KafkaConstants.TYPE).isEqualTo(KafkaConstants.DEBIT)))
+				.to("direct:LoansNoteRouteBuilder.DO_ROLLBACK_CREDIT_DEPOSIT_URI")
+			.endChoice()
+				.when(PredicateBuilder.and(header(KafkaConstants.ACCCOUNT_TYPE).isEqualTo(KafkaConstants.LOANS_NOTE),header(KafkaConstants.TYPE).isEqualTo(KafkaConstants.CREDIT)))
+				.to("direct:LoansNoteRouteBuilder.DO_ROLLBACK_CREDIT_DEPOSIT_URI")
+			.endChoice()
+				.when(PredicateBuilder.and(header(KafkaConstants.ACCCOUNT_TYPE).isEqualTo(KafkaConstants.LOANS_NOTE),header(KafkaConstants.TYPE).isEqualTo(KafkaConstants.DEBIT)))
+				.to("direct:LoansNoteRouteBuilder.DO_ROLLBACK_CREDIT_DEPOSIT_URI")
+			.endChoice()
+		.end()
 	}
 	
 }
